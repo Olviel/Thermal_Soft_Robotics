c9bfa4be0b04e462c1f535afd3b704bc1f8ebb84
O Vaarkamp <oliviervaarkamp@hotmail.nl>
2023-11-10 11:31:09 +0100

    finished plot for current welding method

--- Functions/save_plot_func.py
+++ Functions/save_plot_func.py
@@ -15,6 +15,7 @@ def save_plot(name):
     # Create the folder if it doesn't exist
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
+    print(folder_path)
 
     # Create the filename with the folder path, time string, and given name
     filename = os.path.join(folder_path, f'{time_string}_{name}.svg')
